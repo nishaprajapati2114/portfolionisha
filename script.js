8782968122fde69eb97d07890938d4e10fb11c2d
nishaprajapati2114 <129456878+nishaprajapati2114@users.noreply.github.com>
2024-03-30 22:40:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+ 
+  document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+  });
+  
